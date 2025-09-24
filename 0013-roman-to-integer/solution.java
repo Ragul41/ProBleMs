833ccb4fddad74b1c42e0fd535a30fe595d57f62
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2025-09-24 07:23:18 +0000

    Sync LeetCode submission Runtime - 3 ms (82.11%), Memory - 44.7 MB (84.41%)

---
+++ 0013-roman-to-integer/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public static int value(char c){
+        switch(c){
+            case('I'):
+            return 1;
+            
+            case('V'):
+            return 5;
+
+            case('X'):
+            return 10;
+
+            case('L'):
+            return 50;
+
+            case('C'):
+            return 100;
+
+            case('D'):
+            return 500;
+
+            case('M'):
+            return 1000;
+
+            default:
+            return 0;
+        }
+    }
+    public int romanToInt(String s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int cur=value(s.charAt(i));
+            int next=(i+1)<n?value(s.charAt(i+1)):0;
+
+            if(cur<next){
+                sum-=cur;
+            }
+            else{
+                sum+=cur;
+            }
+        }
+        return sum;
+    }
+} 
