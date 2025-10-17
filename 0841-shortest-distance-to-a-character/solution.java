d537d08e24ed826e517cb4f31defdc39435c6fbb
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2025-10-17 15:23:00 +0000

    Sync LeetCode submission Runtime - 3 ms (32.52%), Memory - 42.5 MB (69.75%)

--- 0841-shortest-distance-to-a-character/solution.java
+++ 0841-shortest-distance-to-a-character/solution.java
@@ -1,48 +1,45 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
-        int n=s.length();
-        int count1=0,count2=0;
-        
-        int []arr=new int[n];
-        for(int i=0;i<n;i++){
-            count1=0;
-            count2=0;
-            int c1=0,c2=0;
+          int n=s.length();
+          int []arr=new int[n];
+          int count1=0,count2=0;
+          int c1=0,c2=0;
+          for(int i=0;i<n;i++){
+            c1=0;
+            c2=0;
             if(s.charAt(i)==c){
-                arr[i]=0;
-                continue;
+              arr[i]=0;
+              continue;
             }
+            for(int j=i;j<n;j++){
+                if(s.charAt(j)==c){
+                    count1=j-i;
+                    c1=1;
+                    break;
+                }
 
-          for(int j=i;j<n;j++){
+            }
+            for(int k=i;k>=0;k--){
+                if(s.charAt(k)==c){
+                    count2=i-k;
+                    c2=1;
+                    break;
+                }
+
+            }
+            if(c1==1&&c2==1){
+                arr[i]=Math.min(count1,count2);
+
+            }
+            else if(c1==1){
+                arr[i]=count1;
+            }
+            else{
+                arr[i]=count2;
+            }
             
-             if(s.charAt(j)==c ){
-                count1=j-i;
-                c1=1;
-                break;
-                
-                
-             }
-
           }
-          for(int k=i;k>=0;k--){
-           if(s.charAt(k)==c){
-            count2=i-k;
-            c2=1;
-            break;
-           }
+          return arr;
 
-          }
-        
-        if(c1==1&&c2==1){
-            arr[i]=Math.min(count1,count2);
-        }
-        else if(c1==1){
-            arr[i]=count1;
-        }
-        else{
-            arr[i]=count2;
-        }
-        }
-        return arr;        
     }
 }
