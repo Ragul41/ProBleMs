d58358e5d5510f9253c91fb20376fe618a79e5d0
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2025-12-18 09:37:30 +0000

    Sync LeetCode submission Runtime - 329 ms (5.10%), Memory - 47.3 MB (5.98%)

--- 0003-longest-substring-without-repeating-characters/solution.java
+++ 0003-longest-substring-without-repeating-characters/solution.java
@@ -6,8 +6,7 @@ class Solution {
         if(!s.isEmpty()&&s.isBlank()||n==1){
             return 1;
         }
-        
-
+        //Stringbuilder s=new Stringbuilder();
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
               char c=s.charAt(j);
@@ -15,14 +14,16 @@ class Solution {
                 s2=s2+c;
               }
               else{
-                int l2=s2.length();
-                if(l2>max){
-                    max=l2;
-                }
+                // int l2=s2.length();
+                // if(l2>max){
+                //     max=l2;
+                // }
+                max=Math.max(max,s2.length());
                 s2="";
                 break;
               }
             }
+            
         }  
         return max;      
     }
