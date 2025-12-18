133dece98a93e01119280419bf4a470e7dcb493f
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2025-12-18 06:22:14 +0000

    Sync LeetCode submission Runtime - 9 ms (5.21%), Memory - 46.9 MB (5.25%)

--- 0014-longest-common-prefix/solution.java
+++ 0014-longest-common-prefix/solution.java
@@ -5,17 +5,18 @@ class Solution {
       String s=strs[0];
       //String s1="";
     for(int i=1;i<l;i++){
-         String s1="";
+        String s1="";
         String s2=strs[i];
         int l1=s.length();
         int l2=s2.length();
-        int l3;
-        if(l1<l2){
-            l3=l1;
-        }
-        else{
-            l3=l2;
-        }
+        int l3=Math.min(l1,l2);
+        // if(l1<l2){
+        //     l3=l1;
+        // }
+        // else{
+        //     l3=l2;
+        // }
+
         for(int j=0;j<l3;j++){
            if(s.charAt(j)==s2.charAt(j)){
              s1=s1+s.charAt(j);
