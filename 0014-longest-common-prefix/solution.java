c91b69c299635923548cb80061315f7541ddfcd2
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2025-12-23 09:11:32 +0000

    Sync LeetCode submission Runtime - 9 ms (5.08%), Memory - 46.5 MB (5.62%)

--- 0014-longest-common-prefix/solution.java
+++ 0014-longest-common-prefix/solution.java
@@ -2,34 +2,23 @@ import java.util.*;
 class Solution {
     public String longestCommonPrefix(String[] strs) {
       int l=strs.length;
-      String s=strs[0];
-      //String s1="";
-    for(int i=1;i<l;i++){
-        String s1="";
-        String s2=strs[i];
-        int l1=s.length();
-        int l2=s2.length();
-        int l3=Math.min(l1,l2);
-        // if(l1<l2){
-        //     l3=l1;
-        // }
-        // else{
-        //     l3=l2;
-        // }
-
-        for(int j=0;j<l3;j++){
-           if(s.charAt(j)==s2.charAt(j)){
-             s1=s1+s.charAt(j);
-           }
-           else{
-            break;
-           }
-           
-        }
-        s=s1;
+      String s1=strs[0];
+     for(int i=1;i<strs.length;i++){
+        int l1=Math.min(s1.length(),strs[i].length());
+        String s2="";
         
+        for(int j=0;j<l1;j++){
+            if(s1.charAt(j)==strs[i].charAt(j)){
+                s2=s2+s1.charAt(j);
+            }
+            else{
+                break;
+            }
+        }
+        s1=s2;
       }
-      return s;
+      
+      return s1;
     }
 }
 
