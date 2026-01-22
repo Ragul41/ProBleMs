9e1f55f2da2482f9ddb5b06369a93774b265237d
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2026-01-22 18:03:38 +0000

    Sync LeetCode submission Runtime - 2 ms (56.20%), Memory - 43.3 MB (36.26%)

---
+++ 0874-backspace-string-compare/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+      
+        
+        String s1=build(s);
+        String s2=build(t);
+
+        if(s1.equals(s2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    private String build(String s){
+        Stack<Character> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='#'){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(!st.isEmpty())
+                st.pop();
+            }
+        }
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        
+        return sb.reverse().toString();
+        }
+        
+}
