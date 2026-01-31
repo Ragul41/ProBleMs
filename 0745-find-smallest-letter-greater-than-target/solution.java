d697a279f367e1aa173bd701cc021f26d84991ab
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2026-01-31 18:05:27 +0000

    Sync LeetCode submission Runtime - 2 ms (3.37%), Memory - 47.4 MB (11.98%)

---
+++ 0745-find-smallest-letter-greater-than-target/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+     Arrays.sort(letters);
+     if(target<letters[0]||target>letters[letters.length-1]||target==letters[letters.length-1]){
+        return letters[0];
+     }
+     else{
+        int i=0;
+        while(target>=letters[i]){
+            i++;
+        }
+        return letters[i];
+     }
+    
+    }
+}
