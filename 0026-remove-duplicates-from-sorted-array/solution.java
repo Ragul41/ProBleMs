e77401ec69325d552716d3bda11d1fa586a46732
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2026-02-02 09:25:55 +0000

    Sync LeetCode submission Runtime - 1 ms (77.22%), Memory - 46.8 MB (36.13%)

--- 0026-remove-duplicates-from-sorted-array/solution.java
+++ 0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,27 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=0;
-        int n=nums.length;
-        for(int j=0;j<n;j++){
-         if(nums[i]!=nums[j]){
-            i++;
-            nums[i]=nums[j];
-         }
+     if(nums.length==0)return 0;
+     int n1=0;
+     for(int i=1;i<nums.length;i++){
+        if(nums[i]!=nums[n1]){
+            n1++;
+            nums[n1]=nums[i];  
+            //n1++;
         }
-        System.out.print("[");
-        for(int k=0;k<i+1;k++){
-            System.out.println(nums[k]);
-            if(k<i){
-                System.out.print(",");
-            }
-        }
-        System.out.print("]");
-
-        
-        
-        
-     
-        return i+1;
+     }
+     return n1+1;
 
 
         
