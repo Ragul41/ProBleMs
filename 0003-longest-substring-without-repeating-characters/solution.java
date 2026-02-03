174d587903a12fe2ea4ce2f6ad9c6188ece384ff
Ragul41 <163963534+Ragul41@users.noreply.github.com>
2026-02-03 05:14:18 +0000

    Sync LeetCode submission Runtime - 6 ms (68.34%), Memory - 46.7 MB (18.83%)

--- 0003-longest-substring-without-repeating-characters/solution.java
+++ 0003-longest-substring-without-repeating-characters/solution.java
@@ -1,30 +1,42 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n=s.length();
-        String s2="";
         int max=0;
-        if(!s.isEmpty()&&s.isBlank()||n==1){
-            return 1;
-        }
-        //Stringbuilder s=new Stringbuilder();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-              char c=s.charAt(j);
-              if(s2.indexOf(c)==-1){
-                s2=s2+c;
-              }
-              else{
-                // int l2=s2.length();
-                // if(l2>max){
-                //     max=l2;
-                // }
-                max=Math.max(max,s2.length());
-                s2="";
-                break;
-              }
-            }
+        int start=0;
+        // String s2="";
+        
+        // if(!s.isEmpty()&&s.isBlank()||n==1){
+        //     return 1;
+        // }
+        // //Stringbuilder s=new Stringbuilder();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //       char c=s.charAt(j);
+        //       if(s2.indexOf(c)==-1){
+        //         s2=s2+c;
+        //       }
+        //       else{
+        //         // int l2=s2.length();
+        //         // if(l2>max){
+        //         //     max=l2;
+        //         // }
+        //         max=Math.max(max,s2.length());
+        //         s2="";
+        //         break;
+        //       }
+        //     }
             
-        }  
+        // } 
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<n;i++){
+           while(set.contains(s.charAt(i))){
+            set.remove(s.charAt(start));
+            start++;
+           }
+           set.add(s.charAt(i));
+           max=Math.max(max,i-start+1);
+        } 
+
         return max;      
     }
 }
